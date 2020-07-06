a69d1214e0f806ff8c8af38f06850ffd1b878a21
GBarnsley <63357847+GBarnsley@users.noreply.github.com>
2020-07-06 23:47:40 +0100

    Corrected so it works when the amount of change >1

--- R/metric-Mean Squared Jump Distance.R
+++ R/metric-Mean Squared Jump Distance.R
@@ -3,7 +3,7 @@
 meanSquaredJumping <- function(epiModel){
   distances <- diff(epiModel@Samples[,-c(1,2)])
   for(i in 1:nrow(distances)){
-    distances[i,] <- cumsum(distances[i,])
+    distances[i,] <- cumsum(distances[i,]) != 0
   }
   distances <- rowSums(distances)
   return(mean(distances^2))
